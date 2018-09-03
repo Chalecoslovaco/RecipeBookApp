1ae7d6c7b92fb7b36e15d3f17ef798b229b60ff9
Sergio García <sergiogarher@gmail.com>
2018-09-03 18:33:48 +0200

    Add ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,21 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     selectedRecipe = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('NameTest', 'DescTest', 'http://www.cookuk.co.uk/images/children_spaghetti_face/children-recipe-pic1-smaller.gif'),
-        new Recipe('NameTest1', 'DescTest1', 'http://www.cookuk.co.uk/images/children_spaghetti_face/children-recipe-pic1-smaller.gif')
+        new Recipe(
+            'NameTest', 
+            'DescTest', 
+            'http://www.cookuk.co.uk/images/children_spaghetti_face/children-recipe-pic1-smaller.gif',
+            [new Ingredient('Rice', 1), new Ingredient('Potatoes', 20)]),
+        new Recipe(
+            'NameTest1', 
+            'DescTest1', 
+            'http://www.cookuk.co.uk/images/children_spaghetti_face/children-recipe-pic1-smaller.gif', 
+            [new Ingredient('Meat', 4), new Ingredient('Tomatoes', 5)])
     ];
 
     getRecipes() {
