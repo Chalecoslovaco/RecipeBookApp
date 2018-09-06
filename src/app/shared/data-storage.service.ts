43ac4a18b280f93dcfec03d30d2e68f296b483f8
Sergio García <sergiogarher@gmail.com>
2018-09-06 10:29:55 +0200

    Transform response data to avoid ingredients error

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
+import { map } from 'rxjs/operators'; 
+
+
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+
 
 @Injectable()
 export class DataStorageService {
@@ -14,9 +18,18 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http.get('https://recipebookapp-94eb0.firebaseio.com/recipes.json').subscribe(
-            (response: Response) =>{
+        this.http.get('https://recipebookapp-94eb0.firebaseio.com/recipes.json')
+        .pipe(map(
+            (response: Response) => {
                 const recipes: Recipe[] = response.json();
+                for (let recipe of recipes)
+                    if(!recipe['ingredients']) recipe['ingredients'] = [];
+                
+                return recipes;
+            }
+        ))
+        .subscribe(
+            (recipes: Recipe[]) =>{
                 this.recipeService.setRecipes(recipes);
             }
         );
