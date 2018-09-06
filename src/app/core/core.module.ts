23a720201766ec43da4bef798c2d3897e08cbe69
Sergio García <sergiogarher@gmail.com>
2018-09-06 17:25:07 +0200

    Merge commit

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,13 +4,10 @@ import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { SharedModule } from "../shared/shared.module";
 import { AppRoutingModule } from "../app-routing.module";
-<<<<<<< HEAD
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { RecipeService } from "../recipes/recipe.service";
 import { DataStorageService } from "../shared/data-storage.service";
 import { AuthService } from "../auth/auth.service";
-=======
->>>>>>> 43f4110b599509ad0d0486d5cde6ed70327f6ab5
 
 @NgModule({
     declarations: [
@@ -24,15 +21,12 @@ import { AuthService } from "../auth/auth.service";
     exports: [
         AppRoutingModule,
         HeaderComponent
-<<<<<<< HEAD
     ],
     providers: [
         ShoppingListService, 
         RecipeService, 
         DataStorageService, 
         AuthService
-=======
->>>>>>> 43f4110b599509ad0d0486d5cde6ed70327f6ab5
     ]
 })
 export class CoreModule {}
